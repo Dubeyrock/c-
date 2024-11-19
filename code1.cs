a7f673adf0a8a1241edc354eb06831e4724a960a
Shivam Dubey <96882359+Dubeyrock@users.noreply.github.com>
2024-11-19 16:50:39 +0530

    Create code1.cs

---
+++ code1.cs
@@ -0,0 +1,13 @@
+// C# program to illustrate if statement
+using System;
+
+public class GFG {
+
+	public static void Main(string[] args)
+	{
+		string name = "Geek";
+		if (name == "Geek") {
+			Console.WriteLine("GeeksForGeeks");
+		}
+	}
+}
